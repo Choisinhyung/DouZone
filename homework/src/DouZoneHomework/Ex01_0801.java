3e9aca5480374dc15fb4cb8662614b8fedf57de7
최신형 <tlsgudrhfem@naver.com>
2022-08-02 09:10:27 +0900

    .

--- homework/src/DouZoneHomework/Ex01_0801.java
+++ homework/src/DouZoneHomework/Ex01_0801.java
@@ -4,17 +4,36 @@ public class Ex01_0801 {
 
 	public static void main(String[] args) {
 		// 부분합 변수 + 숫자 1개씩 추가 / for문 /  => 35 ( 1~5) : 1 + (1+2) + (1+2+3) ...
-		
-		int part = 0;
+	
 		int sum = 0;
 		
 		for(int i = 1; i <= 5; i++) {
-			for(int j = 1; j < i + 1; i++) {
-				System.out.println(i);
-//				part = i + j;
+			for(int j = 1; j <= i; j++) {
+				sum += j;
 			}
 		}
-
+		System.out.println(sum);
+		
+		System.out.println("==================");
+		
+		int sum2 = 0;
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+			 sum2 -= i;
+			} else if ( i % 2 == 0 )
+				sum2 += i;
+		}
+		System.out.println(sum2);
+		
+		System.out.println("==================");
+		
+		double sum3 = 0;
+		
+		for(double i = 1; i < 10; i++) {
+			sum3 += i / (i+1);
+		}
+		System.out.printf("result = %.2f", sum3);
 	}
 
 }
